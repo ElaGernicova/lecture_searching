71ecd6511ecd98183e405015957e9d59d355d3b6
Ela Gernicova <283249@vutbr.cz>
2026-04-15 10:37:11 +0200

    ...

--- searching.py
+++ searching.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import json
+import time
+import matplotlib.pyplot as plt
+from generators import unordered_sequence as plt
 
 
 def read_data(file_name, field):
@@ -55,6 +58,29 @@ def binary_search(searched_data, searched_number):
             right = middle + 1
     return None
 
+def test_complexity(list_of_n):
+    for n in list_of_n:
+        unordered_data = unordered_sequence(n)
+        ordered_data = ordered_sequence(n)
+        duration_linear = 0
+        duration_binary = 0
+        repetitions = 100
+        for measurments in range(repetitions):
+            start_time_linear = time.perf_counter()
+            found_numbers = linear_search(unordered_data, n)
+            end_time_linear = time.perf_counter()
+            duration_linear += end_time_linear - start_time_linear
+
+            start_time_binary = time.perf_counter()
+            found_numbers_bin = binary_search(ordered_data, n)
+            end_time_binary = time.perf_counter()
+            duration_binary += end_time_linear - start_time_linear
+        time_linear.append(duration_linear / repetitions)
+        time_binary.append(duration_binary / repetitions)
+    plt.plot(list_of_n, time_linear)
+    plt.plot(list_of_n, time_binary)
+
+
 
 
 def main():
@@ -62,8 +88,15 @@ def main():
     print(my_data)
     my_data2 = read_data("sequential.json", "ordered_numbers")
     print(my_data2)
+    for measurments in range(repetitions):
+        start_time = time.perf_counter()
+        found_numbers = linear_search(my_data, n)
+        end_time = time.perf_counter()
+        duration += end_time - start_time
     n = 21
+    start_time = time.perf_counter()
     ls = linear_search(my_data, n)
+    end_time = time.perf_counter()
     print(ls)
     bn = binary_search(my_data2, n)
     print(bn)
